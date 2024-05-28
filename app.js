eb2e04f2b314ad3e8fce315d6f17d24251893cf8
Essang547 <essangedet29@gmail.com>
2024-05-28 16:50:54 +0100

    Save

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME:ESSANG ETIM EDET MATRIC NO:VUG/CSC/21/5802 '));
+app.use(express.static("public"));
+
+// app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME:ESSANG ETIM EDET MATRIC NO:VUG/CSC/21/5802 '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
